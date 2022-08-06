cb9b5652248e1531d4515186e079dfe7a623ae4c
angel1254mc <angellopezpol1254@gmail.comm>
2022-08-06 00:18:15 -0400

    added server origin as allowed origin (sts req)

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://www.eventually-admin-portal',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    process.env.PORT ? process.env.PORT : 'https://get-server-prod.herokuapp.com/'
 ]
 module.exports = allowedOrigins;
