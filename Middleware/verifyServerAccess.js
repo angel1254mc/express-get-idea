f2059997b8f43a57846364206e426994a746e184
Angel Lopez Pol <59091763+angel1254mc@users.noreply.github.com>
2022-08-18 13:36:52 -0400

    Updated verifyServerAccess, now works

--- Middleware/verifyServerAccess.js
+++ Middleware/verifyServerAccess.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyServerAccess = (req, res, next) => {
-    const API_key = req.query.API_key;
-    if (API_key === process.env.SERVER_API_KEY)
+    const API_key = req.body.SERVER_SECRET_API_KEY
+    if (API_key === process.env.SERVER_SECRET_API_KEY)
         return next();
     else
         return res.status(403).json({message: "Forbidden. API key incorrect or not included"});
